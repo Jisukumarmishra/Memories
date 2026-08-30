9b3fa939a09a21e2168098bf204ef0c31144839a
mishra <jisumishra4@gmail.com>
2026-08-30 21:10:20 +0530

    Add Post Componenet

--- client/src/componenets/Posts/Posts.js
+++ client/src/componenets/Posts/Posts.js
@@ -1,9 +1,26 @@
-import React from 'react'
-import Post from './Post/Post';
-  
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Post from "./Post/Post";
+import { getPosts } from "../../redux/features/posts/postsSlice";
+
 const Posts = () => {
+  const dispatch = useDispatch();
+
+  const posts = useSelector((state) => state.posts.posts);
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
   return (
-    <><div>POSTS</div></>
-  )
-} 
-export default Posts
+    <>
+      <div>
+        {posts.map((post) => (
+          <Post key={post._id} post={post} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Posts;
