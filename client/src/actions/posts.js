bcb3a9df07832a9ff5f82d91598133055103bb34
mishra <jisumishra4@gmail.com>
2026-09-09 15:22:08 +0530

    Add LikePost Action

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -37,3 +37,13 @@ export const deletepost = (id) => async (dispach) => {
   console.log(error);
   }
 }
+
+export const likePost = (id) => async (dispatch) => {
+  try {
+  const { data } = await api.likePost(id);
+  dispatch({ type: 'LIKE', payload: data });
+  } catch (error) {
+console.log(error);
+  }
+
+}
